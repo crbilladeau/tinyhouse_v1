425a3b212b72f92784d39d2b395c8721922a80ea
Charlie Billadeau <charlie.billadeau@gmail.com>
2021-03-02 21:54:20 -0600

    finished adding menu and submenu functionality for app header

---
+++ client/src/sections/Login/index.tsx
@@ -0,0 +1,131 @@
+import React, { useEffect, useRef } from 'react';
+import { Redirect } from 'react-router';
+import { useApolloClient, useMutation } from '@apollo/client';
+import { Card, Layout, Spin, Typography } from 'antd';
+import { ErrorBanner } from '../../lib/components';
+import { AUTH_URL } from '../../lib/graphql/queries';
+import { LOG_IN } from '../../lib/graphql/mutations';
+import {
+  LogIn as LogInData,
+  LogInVariables,
+} from '../../lib/graphql/mutations/LogIn/__generated__/LogIn';
+import { AuthUrl as AuthUrlData } from '../../lib/graphql/queries/AuthUrl/__generated__/AuthUrl';
+import { Viewer } from '../../lib/types';
+import {
+  displaySuccessNotification,
+  displayErrorMessage,
+} from '../../lib/utils';
+
+// Image Assets
+import googleLogo from './assets/google_logo.jpg';
+
+// setViewer passed down from app index.tsx
+interface Props {
+  setViewer: (viewer: Viewer) => void;
+}
+
+// antd stylings
+const { Content } = Layout;
+const { Text, Title } = Typography;
+
+export const Login = ({ setViewer }: Props) => {
+  // connect to apollo client
+  const client = useApolloClient();
+  // stores our logIn data in a useMutation hook
+  const [
+    logIn,
+    { data: logInData, loading: logInLoading, error: logInError },
+  ] = useMutation<LogInData, LogInVariables>(LOG_IN, {
+    onCompleted: (data) => {
+      if (data && data.logIn) {
+        setViewer(data.logIn);
+        displaySuccessNotification("You've succesfully logged in!");
+      }
+    },
+  });
+
+  // useRef allows us to avoid having to pass it into useEffect as a dependency bc it will change on each render
+  const logInRef = useRef(logIn);
+
+  useEffect(() => {
+    // get the current 'code' param from the url
+    const code = new URL(window.location.href).searchParams.get('code');
+
+    if (code) {
+      logInRef.current({
+        variables: {
+          input: { code },
+        },
+      });
+    }
+  }, []);
+
+  // sign in with google handler
+  const handleAuthorize = async () => {
+    try {
+      // get the user data from the AuthUrl query
+      const { data } = await client.query<AuthUrlData>({
+        query: AUTH_URL,
+      });
+      // check if it matches the params
+      window.location.href = data.authUrl;
+    } catch (error) {
+      displayErrorMessage(
+        "Sorry! We weren't able to log you in. Please try again later!"
+      );
+    }
+  };
+
+  // spinner
+  if (logInLoading) {
+    return (
+      <Content>
+        <Spin size='large' tip='Logging you in...' />
+      </Content>
+    );
+  }
+
+  if (logInData && logInData.logIn) {
+    const { id: viewerId } = logInData.logIn;
+    return <Redirect to={`/user/${viewerId}`} />;
+  }
+
+  const logInErrorBannerElement = logInError ? (
+    <ErrorBanner description="Sorry! We weren't able to log you in. Please try again later!" />
+  ) : null;
+
+  return (
+    <Content className='log-in'>
+      {logInErrorBannerElement}
+      <Card className='log-in-card'>
+        <div className='log-in-card__intro'>
+          <Title level={3} className='log-in-card__intro-title'>
+            <span role='img' aria-label='wave'>
+              👋
+            </span>
+          </Title>
+          <Title level={3} className='log-in-card__intro-title'>
+            Log into TinyHouse
+          </Title>
+          <Text>Sign in with Google to start booking available rentals!</Text>
+        </div>
+        <button
+          className='log-in-card__google-button'
+          onClick={handleAuthorize}>
+          <img
+            src={googleLogo}
+            alt='Google Logo'
+            className='log-in-card__google-button-logo'
+          />
+          <span className='log-in-card__google-button-text'>
+            Sign in with Google
+          </span>
+        </button>
+        <Text type='secondary'>
+          Note: By signing in, you'll be redirected to the Google consent form
+          to sign in with your Google account.
+        </Text>
+      </Card>
+    </Content>
+  );
+};
